fedb976aae9c3b4beb5e8a87654d0c4354e69b7b
Ujjwal Jain <ujjwal.jain116@gmail.com>
2022-09-07 16:47:10 +0530

    Create homepage, fetch and display all character's image and their name

---
+++ script.js
@@ -0,0 +1,88 @@
+var hash = '860b056faebcf3f7b371c6b40515e37c';
+var key = '?ts=1&apikey=f584d5df205ec1d450f443087814077d&hash=860b056faebcf3f7b371c6b40515e37c';
+var container = document.querySelector('#container');
+
+
+function getAllCharacters(){
+
+    var myRequest = new XMLHttpRequest();
+
+    myRequest.onload = function(){
+        
+        var requestResponse = myRequest.response;
+        var JSONresponse = JSON.parse(requestResponse);        
+        
+        var length = JSONresponse.data.results.length;
+        
+        for(let i=0; i<length ; i++){
+        var imageURL = JSONresponse.data.results[i].thumbnail.path + '.' +JSONresponse.data.results[i].thumbnail.extension  ;
+        var charName = JSONresponse.data.results[i].name;
+        let id = JSONresponse.data.results[i].id;
+
+        // console.log(imageURL);
+
+        //create new elements
+        var newDiv = container.appendChild(document.createElement('div'));
+        newDiv.setAttribute('class', 'characters');
+        var favDiv= newDiv.appendChild(document.createElement('div'));
+        favDiv.setAttribute('class', 'fav');
+        favDiv.innerHTML = '<i class="fa-solid fa-heart"></i>' ;
+        var anchorTag = newDiv.appendChild(document.createElement('a'));
+        anchorTag.setAttribute('href', '/marvel/try.html?id='+id);
+        var imgTag = anchorTag.appendChild(document.createElement('img'));
+        imgTag.setAttribute("src", imageURL);
+        anchorTag.appendChild(document.createElement('p')).innerText = charName;
+        
+        // favClicked = document.querySelectorAll('.fav');
+        // // console.log(favClicked[10]);
+        
+        // favClicked[i].addEventListener('click', function(){
+        //     favClicked[i].classList.toggle('fav-clicked');
+            
+        //     let flag=true;
+        //     let index;
+        //     for(let i=0; i<arr.length; i++){
+        //     // for(let a of arr){
+        //         if( id == arr[i]){
+        //             flag = false;
+        //             index = i;
+        //             break;
+        //         }
+        //         else{
+        //             flag = true;
+        //         }
+        //     }
+            
+        //     console.log(flag);
+        //     if(flag){
+        //     arr.push(id);
+        //     }
+        //     else{
+        //         console.log(index);
+        //         arr.splice(index,1);
+        //     }
+            
+            
+        //     console.log(arr);
+        //     localStorage.setItem('name', JSON.stringify(arr));
+        //     // localStorage.setItem('name', arr);
+        //     });
+        
+        }
+        
+    }
+
+    myRequest.onerror = function(xhr, textStatus, errorThrown){
+        console.log('Error Occured');
+                    
+    };
+    
+    myRequest.open('get', 'https://gateway.marvel.com/v1/public/characters'+ key, true);
+    myRequest.send();
+}
+
+
+
+
+
+window.onload = getAllCharacters;
